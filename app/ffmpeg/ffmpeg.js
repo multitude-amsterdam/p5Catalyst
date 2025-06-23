47bf081ee91d73ddbd8f0ae71cdc4156b17dd89d
Aidan Wyber <pandakaas@gmail.com>
2025-06-23 13:42:05 +0200

    fixed core loading

--- app/ffmpeg/ffmpeg.js
+++ app/ffmpeg/ffmpeg.js
@@ -1,6 +1,4 @@
 
-const doLoadLocal = true;
-
 // example from:
 // https://github.com/ffmpegwasm/ffmpeg.wasm/blob/main/apps/vanilla-app/public/concatDemuxer.html
 
@@ -42,28 +40,21 @@ const WEBM_TRANSPARENT = {
 let ffmpegExportSettings;// = MP4;
 
 
-
 function startCapture() {
-	// console.log('startCapture()...');
 	savedFrameCount = 0;
 	isCapturingFrames = true;
 	isPlaying = true;
 	conversionProgress = 0;
-	// frameCount = 0; // resets 'time'
 }
 
 
-
 function stopCapture() {
-	// console.log('stopCapture()...');
 	isCapturingFrames = false;
 	isPlaying = false;
 }
 
 
-
 async function clearFramesDir() {
-	// console.log('clearFramesDir()...');
 	const listdir = await ffmpeg.listDir('/frames');
 	for (let item of listdir) {
 		if (item.isDir) continue;
@@ -71,7 +62,6 @@ async function clearFramesDir() {
 	}
 	await ffmpeg.deleteDir('/frames');
 }
-
 
 
 const BASE64_MARKER = ';base64,';
@@ -95,38 +85,34 @@ function saveToLocalFFMPEG(frameId) {
 }
 
 
-
 function ffmpegUtilInit() {
 	guiCaptureButtonMP4 = gui.getController('buttonVidCaptureMP4');
 	guiCaptureButtonWEBM = gui.getController('buttonVidCaptureWEBM');
 }
 
 
-
 async function ffmpegInit() {
 	ffmpeg = new FFmpeg();
 
-	ffmpeg.on("log", ({ logMsg }) => {
+	ffmpeg.on('log', ({ logMsg }) => {
 		// console.log(logMsg);
 		updateConversionProgress();
 	});
 
-	ffmpeg.on("progress", ({ progress, time }) => {
+	ffmpeg.on('progress', ({ progress, time }) => {
 		// console.log(progress, time);
 		updateConversionProgress(progress);
 	});
 
+	let dirPath = window.location.pathname;
+	if (dirPath.indexOf('/') > -1)
+		dirPath = dirPath.substring(0, dirPath.lastIndexOf('/'));
 	await ffmpeg.load({
-		// INCLUDE ROOT OF DOMAIN
-		coreURL: (doLoadLocal ?
-			(window.location.pathname + "ffmpeg/core/ffmpeg-core.js") : // LOCAL
-			(window.location.origin + "/shared-libs/ffmpeg/core/ffmpeg-core.js") // SHARED
-		)
+		coreURL: dirPath + '/ffmpeg/core/ffmpeg-core.js'
 	});
 
 	isFfmpegInit = true;
 }
-
 
 
 function ffmpegSetup() {
@@ -135,13 +121,7 @@ function ffmpegSetup() {
 }
 
 
-
 function updateConversionProgress(progress=undefined) {
-	// conversionProgress += dConversionProgress;
-	// let s = '';
-	// let n = conversionProgress % 30;// round(nmc(conversionProgress) * 25);
-	// for (let i = 0; i < n; i++) s += '#';
-	// guiCaptureButtonChoice.controllerElement.html(s);
 	if (progress === undefined) return;
 	
 	let progText;
@@ -157,46 +137,35 @@ function updateConversionProgress(progress=undefined) {
 }
 
 
-
 function ffmpegSaveFrame(frameId, pngData) {
 	ffmpeg.createDir('frames');
 	let fileName = nf(frameId, 6) + '.png';
 	let filePath = 'frames/' + fileName;
 	ffmpeg.writeFile(filePath, pngData);
-	// console.log(`Written ${filePath}.`);
 }
-
 
 
 async function getFrameFileNames() {
 	let frames = await ffmpeg.listDir('frames');
 	frames = frames.filter(item => !item.isDir).map(frame => frame.name);
-	// console.log(frames);
 	return frames;
 }
 
 
-
 async function ffmpegCreateMP4() {
-	// console.log('Creating MP4...');
-
 	guiCaptureButtonChoice.disable();
 	guiVideoLoadingDiv.div.show();
 
 	// create concatenation file list as .txt
 	let frames = await getFrameFileNames();
 	let inputPaths = frames.map(f => `file frames/${f}`);
-	// console.log(inputPaths);
 	const concatFile = 'concat_list.txt';
 	await ffmpeg.writeFile(concatFile, inputPaths.join('\n'));
 
 	// run ffmpeg concatenation
 	const outputFile = 'output.' + ffmpegExportSettings.ext;
-	// await ffmpeg.exec(['-f', 'concat', '-safe', '0', '-i', concatFile, 'output.mp4']);
-	// const args = `-r 30 -f image2 -safe 0 -f concat -i ${concatFile} -progress pipe:2 -vcodec libx264 -pix_fmt yuv420p -crf 21 -vf fps=30.0,scale=1080:1080:flags=lanczos -movflags faststart ${outputFile}`.split(' ');
 	const width = canvas.width, height = canvas.height;
 	
-	// MP4 working:
 	let args;
 	switch (ffmpegExportSettings.ext) {
 	case 'mp4':
@@ -208,18 +177,14 @@ async function ffmpegCreateMP4() {
 
 	}
 
-	print(args.join(' '));
+	print('ffmpeg.wasm arguments:', args.join(' '));
 
 	let execResult = await ffmpeg.exec(args);
-	// console.log(`ffmpeg.exec result: ${execResult}`);
-	// console.log('Completed concatenation.');
 
 	// load mp4 to HTML video element
-	// console.log('Loading MP4 data...');
 	const data = await ffmpeg.readFile(outputFile);
 	const video = document.getElementById('output-video');
 	const blob = new Blob([data.buffer], {type: ffmpegExportSettings.mimeType});
-	console.log(blob);
 	const blobURL = URL.createObjectURL(blob);
 	video.src = blobURL;
 	
@@ -234,17 +199,14 @@ async function ffmpegCreateMP4() {
 	guiCaptureButtonChoice.controllerElement.html(lang.process('Start LANG_VID_CAPTURE ' + ffmpegExportSettings.ext.toUpperCase(), true));
 	guiCaptureButtonChoice.enable();
 
-	// setTimeout(() => alert('De video is gereed en wordt gedownload.'), 50);
 	setTimeout(() => alert(lang.process('LANG_VIDEO_READY_MSG', true)), 50);
 
-	// console.log('Clearing frames...');
 	clearFramesDir();
 
 	console.log('Created and loaded video.');
 
 	isPlaying = true;
 }
-
 
 
 async function dir(path) {
