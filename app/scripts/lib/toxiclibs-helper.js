b314b94938c93422ba64e2a5f8bcb4ace9991baf
Aidan Wyber <pandakaas@gmail.com>
2025-07-14 12:02:47 +0200

    neater by ES6 sugar

--- app/scripts/lib/toxiclibs-helper.js
+++ app/scripts/lib/toxiclibs-helper.js
@@ -1,30 +1,26 @@
-const Rect = toxi.geom.Rect;
-const Circle = toxi.geom.Circle;
-const Polygon2D = toxi.geom.Polygon2D;
+const {
+	Rect,
+	Circle,
+	Polygon2D,
+	Vec2D,
+	Line2D,
+	Ray2D,
+	Vec3D,
+	Line3D,
+	Ray3D,
+	Quaternion,
+} = toxi.geom;
 
-const Vec2D = toxi.geom.Vec2D;
-const Line2D = toxi.geom.Line2D;
-const Ray2D = toxi.geom.Ray2D;
+const {
+	VerletPhysics2D,
+	GravityBehavior2D,
+	AttractionBehavior2D,
+	VerletParticle2D,
+	VerletSpring2D,
+	VerletMinDistanceSpring2D,
+} = toxi.physics2d;
 
-const Vec3D = toxi.geom.Vec3D;
-const Line3D = toxi.geom.Line3D;
-const Ray3D = toxi.geom.Ray3D;
+const { AttractionBehavior, ConstantForceBehavior, GravityBehavior } =
+	toxi.physics2d.behaviors;
 
-const Quaternion = toxi.geom.Quaternion;
-
-const SimplexNoise = toxi.math.noise.simplexNoise;
-const simplexNoise = SimplexNoise.noise;
-
-const VerletPhysics2D = toxi.physics2d.VerletPhysics2D;
-const GravityBehavior2D = toxi.physics2d.behaviors.GravityBehavior;
-const AttractionBehavior2D = toxi.physics2d.behaviors.AttractionBehavior;
-const VerletParticle2D = toxi.physics2d.VerletParticle2D;
-const VerletSpring2D = toxi.physics2d.VerletSpring2D;
-const VerletMinDistanceSpring2D = toxi.physics2d.VerletMinDistanceSpring2D;
-
-// const VerletPhysics3D = toxi.physics3d.VerletPhysics3D;
-// const GravityBehavior3D = toxi.physics3d.behaviors.GravityBehavior;
-// const AttractionBehavior3D = toxi.physics3d.behaviors.AttractionBehavior;
-// const VerletParticle3D = toxi.physics3d.VerletParticle3D;
-// const VerletSpring3D = toxi.physics3d.VerletSpring3D;
-// const VerletMinDistanceSpring3D = toxi.physics3d.VerletMinDistanceSpring3D;
+const simplexNoise = toxi.math.noise.simplexNoise.noise;
