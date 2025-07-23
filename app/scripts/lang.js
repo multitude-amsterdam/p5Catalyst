1047a53e1e2c2b65eb44c35a5536a88c3aacafe4
Aidan Wyber <pandakaas@gmail.com>
2025-07-23 17:29:21 +0200

    headings for alerts

--- app/scripts/lang.js
+++ app/scripts/lang.js
@@ -29,16 +29,16 @@ const language = 'en';
  */
 const dictionary = {
 	LANG_WRONG_FILE_TYPE_MSG: {
-		nl: 'Het verkeerde bestandtype was geselecteerd.',
-		en: 'The wrong file type was selected.',
+		nl: '<h1>Verkeerd bestandstype</h1><p>Het verkeerde bestandtype was geselecteerd.</p>',
+		en: '<h1>Wrong file type</h1><p>The wrong file type was selected.</p>',
 	},
 	LANG_WIDTH: {
 		nl: 'breedte',
 		en: 'width',
 	},
 	LANG_VIDEO_READY_MSG: {
-		nl: 'De video is gereed and wordt gedownload.',
-		en: 'The video is ready and will be downloaded.',
+		nl: '<h1>Video gereed</h1><p>De video is gereed and is gedownload.</p>',
+		en: '<h1>Video ready</h1><p>The video is ready and was downloaded.</p>',
 	},
 	LANG_VIDEO: {
 		nl: 'video',
@@ -134,11 +134,11 @@ const dictionary = {
 	},
 	LANG_TOO_SMALL_IMG_ALERT: {
 		nl:
-			'De afmetingen van de afbeelding ({0} x {1}) zijn te laag voor een mooi optisch effect.\n' +
-			'Kies een afbeelding van ten minste {2} x {3} pixels.',
+			'<h1>Afbeelding te klein</h1><p>De afmetingen van de afbeelding ({0} x {1}) zijn te laag voor een mooi optisch effect.\n' +
+			'Kies een afbeelding van ten minste {2} x {3} pixels.</p>',
 		en:
-			'The dimensions of the image ({0} x {1}) are too low for a good optical effect.\n' +
-			'Choose an image of at least {2} x {3} pixels.',
+			'<h1>Image too small</h1><p>The dimensions of the image ({0} x {1}) are too low for a good optical effect.\n' +
+			'Choose an image of at least {2} x {3} pixels.</p>',
 	},
 	LANG_TOO_SMALL_IMG: {
 		nl: 'Kleiner dan minimum afmetingen: {0} x {1} pixels.',
@@ -149,26 +149,28 @@ const dictionary = {
 			`<h1>Help</h1>` +
 			`<div class="helpme">` +
 			`<ul>` +
-			`<li><span>Laat deze popup zien</span> <span><code>H</code></span></li>` +
+			`<li><span>Geselcteerde controls randomizen</span> <span><code>R</code></span></li>` +
 			`<li><span>Pauzeren / afspelen animatie</span> <span><code>spatiebalk</code></span></li>` +
-			`<li><span>Ongedaan maken / opnieuw</span> <span><code>CTRL / CMD</code> + <code>Z</code></span></li>` +
+			`<li><span>Ongedaan maken</span> <span><code>CTRL / CMD</code> + <code>Z</code></span></li>` +
 			`<li><span>Opnieuw</span> <span><code>CTRL / CMD</code> + <code>SHIFT</code> + <code>Z</code></span></li>` +
 			`<li><span>Volledig scherm</span> <span><code>F</code></span></li>` +
 			`<li><span>Zijbalk verspringen</span> <span><code>B</code></span></li>` +
 			`<li><span>Wissel tussen licht/donker thema</span> <span><code>M</code></span></li>` +
+			`<li><span>Laat deze popup zien</span> <span><code>H</code></span></li>` +
 			`</ul>` +
 			`</div>`,
 		en:
 			`<h1>Help</h1>` +
 			`<div class="helpme">` +
 			`<ul>` +
-			`<li><span>Show this popup</span> <span><code>H</code></span></li>` +
+			`<li><span>Randomize selected controls</span> <span><code>R</code></span></li>` +
 			`<li><span>Pause / play animation</span> <span><code>spacebar</code></span></li>` +
 			`<li><span>Undo</span> <span><code>CTRL / CMD</code> + <code>Z</code></span></li>` +
 			`<li><span>Redo</span> <span><code>CTRL / CMD</code> + <code>SHIFT</code> + <code>Z</code></span></li>` +
 			`<li><span>Toggle fullscreen</span> <span><code>F</code></span></li>` +
 			`<li><span>Flip sidebar</span> <span><code>B</code></span></li>` +
 			`<li><span>Toggle light/dark mode</span> <span><code>M</code></span></li>` +
+			`<li><span>Show this popup</span> <span><code>H</code></span></li>` +
 			`</ul>` +
 			`</div>`,
 	},
