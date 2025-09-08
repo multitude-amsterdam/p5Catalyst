8e4330c48e29e9d7670e33a6efb13dba39c84cd8
Aidan Wyber <pandakaas@gmail.com>
2025-09-08 15:53:58 +0200

    test sketch

--- src/main.js
+++ src/main.js
@@ -1,31 +1,35 @@
 import { catalyst } from './lib';
 
 const sketchFunction = async (sketch, state) => {
-	state.size = 50;
-
 	sketch.setup = async () => {
-		state.color = sketch.color(0);
-
-		sketch.angleMode(sketch.DEGREES);
 		sketch.noStroke();
-		sketch.frameRate(60);
+
+		state.circleColor = sketch.color(0);
+		state.circleSize = 0.5;
+		state.bgColor = sketch.color(0);
+		state.nBgElements = 5;
 	};
 
 	sketch.draw = () => {
-		sketch.fill(0, 0, 255);
-		let sx = state.width / 5;
-		let sy = state.height / 5;
-		for (let x = 0; x < 5; x++) {
-			for (let y = 0; y < 5; y++) {
+		sketch.fill(state.bgColor);
+		let sx = state.width / state.nBgElements;
+		let sy = state.height / state.nBgElements;
+		for (let x = 0; x < state.nBgElements; x++) {
+			for (let y = 0; y < state.nBgElements; y++) {
 				sketch.ellipse((x + 0.5) * sx, (y + 0.5) * sy, sx, sy);
 			}
 		}
 
-		sketch.fill(state.color);
+		sketch.fill(state.circleColor);
+		const diam =
+			sketch.width *
+			sketch.lerp(1 / state.nBgElements, 1, state.circleSize);
+		const amp = (sketch.height - diam) / 2;
 		sketch.circle(
 			state.width / 2,
-			state.height / 2 + sketch.sin(state.time * 100) * 700,
-			state.size
+			state.height / 2 +
+				sketch.sin(state.progress * sketch.TAU * 2) * amp,
+			diam
 		);
 	};
 };
@@ -35,7 +39,7 @@ const plugins = [
 	catalyst.languagePlugin('en', {
 		LANG_SLEEP: { nl: 'slapen', en: 'sleep' },
 	}),
-	catalyst.randomizerPlugin(['slider', 'colorBox']),
+	catalyst.randomizerPlugin(['colorBoxesCircle', 'sliderCircleSize']),
 	catalyst.debugPlugin(),
 ];
 
@@ -44,31 +48,56 @@ catalyst.initialize(
 	(gui, state) => {
 		const appearanceTab = gui.getTab('appearance');
 
-		const panel = appearanceTab.addPanel('Circle');
+		const circlePanel = appearanceTab.addPanel('Circle');
 
-		panel.addColorBoxes(
-			'colorBox',
+		circlePanel.addColorBoxes(
+			'colorBoxesCircle',
 			'Circle color',
-			['#FF6400', '#86D594', '#004D30', '#002835ff', '#8373FF'],
+			['#FF6400', '#86D594', '#004D30', '#0000FF', '#F5CBFF'],
 			0,
 			(controller, value) => {
-				state.color = value;
+				state.circleColor = value;
 			}
 		);
 
-		panel.addSlider(
-			'slider',
+		circlePanel.addSlider(
+			'sliderCircleSize',
 			'Circle size',
+			0,
 			1,
-			500,
-			100,
+			state.circleSize,
+			0.0001,
+			(controller, value) => {
+				state.circleSize = value;
+			}
+		);
+
+		const bgPanel = appearanceTab.addPanel('Background pattern');
+
+		bgPanel.addColorBoxes(
+			'colorBoxesBg',
+			'Background color',
+			['#FF6400', '#86D594', '#004D30', '#0000FF', '#F5CBFF'],
+			3,
+			(controller, value) => {
+				state.bgColor = value;
+			}
+		);
+
+		bgPanel.addSlider(
+			'sliderNBg',
+			'Number of ellipses',
+			1,
+			10,
+			state.nBgElements,
 			1,
 			(controller, value) => {
-				state.size = value;
+				state.nBgElements = value;
 			}
 		);
 
-		panel.open();
+		circlePanel.open();
+		bgPanel.open();
 	},
 	plugins
 );
