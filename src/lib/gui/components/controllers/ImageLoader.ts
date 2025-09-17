b1bba35264be329ae64cae4d86b307ab91ea0dca
Aidan Wyber <pandakaas@gmail.com>
2025-09-17 14:41:46 +0200

    fixed image type
    was loading p5.Element (<img>), is now loading a p5.Image which is more widely used in p5
    was causing an error in one of my generators because of this

--- src/lib/gui/components/controllers/ImageLoader.ts
+++ src/lib/gui/components/controllers/ImageLoader.ts
@@ -2,13 +2,8 @@ import type p5 from 'p5';
 import type { fileReadyCallback, setupCallback } from '../../../types';
 import type { GUIForP5 } from '../../GUIForP5';
 import { FileLoader } from './FileLoader';
-
-/**
- * Loader that converts files to p5.Image instances.
- * @extends FileLoader
- */
 export class ImageLoader extends FileLoader {
-	img?: p5.Element;
+	img?: p5.Image;
 
 	constructor(
 		gui: GUIForP5,
@@ -23,9 +18,9 @@ export class ImageLoader extends FileLoader {
 			labelStr,
 			'image',
 			file => {
-				file = file as p5.File;
-				this.img = gui.p5Instance.createImg(file.data, '');
-				this.img.hide();
+				this.img = gui.p5Instance.loadImage(file.data, img => {
+					fileReadyCallback(img, this);
+				});
 				this.file = this.img;
 			},
 			setupCallback
