dd5d8ec94d02d98358aebc62b6d8536d679c49a4
Aidan Wyber <pandakaas@gmail.com>
2025-10-05 12:33:34 +0200

    revert downstream mistake

--- generators/eden/main.js
+++
@@ -1,9 +0,0 @@
-import '/src/style/inter.css';
-import '/src/style/style.css';
-
-import { Catalyst } from '/src/lib/Catalyst';
-import { sketchSeed } from './sketch';
-import { createGui } from './create-gui';
-import { plugins } from './create-plugins';
-
-new Catalyst(sketchSeed, createGui, plugins);
